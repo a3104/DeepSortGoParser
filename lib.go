f8145dde0da3d5a90fc0cce07c707a48dabb0c4d
a3104 <a3104@jaaku.org>
2021-04-13 19:41:53 +0900

    init

---
+++ lib.go
@@ -0,0 +1,64 @@
+package deepsortParser
+
+import (
+	"strconv"
+	"strings"
+)
+
+type DeepSort struct {
+	FrameNo  int
+	PosX     float64
+	PosY     float64
+	PosW     float64
+	PosH     float64
+	Category float64
+	//Uoverdot float64
+	//Voverdot float64
+	//Soverdot float64
+	Identity float64
+}
+
+func CreateByLine(line string) (DeepSort, error) {
+	parts := strings.Split(line, ",")
+	var deepSort DeepSort
+
+	if frameNo, error := strconv.Atoi(parts[0]); error == nil {
+		deepSort.FrameNo = frameNo
+	} else {
+		return DeepSort{}, error
+	}
+	if PosX, error := strconv.ParseFloat(parts[1], 64); error == nil {
+		deepSort.PosX = PosX
+	} else {
+		return DeepSort{}, error
+	}
+	if PosY, error := strconv.ParseFloat(parts[2], 64); error == nil {
+		deepSort.PosY = PosY
+	} else {
+		return DeepSort{}, error
+	}
+	if PosW, error := strconv.ParseFloat(parts[3], 64); error == nil {
+		deepSort.PosW = PosW
+	} else {
+		return DeepSort{}, error
+	}
+	if PosH, error := strconv.ParseFloat(parts[4], 64); error == nil {
+		deepSort.PosH = PosH
+	} else {
+		return DeepSort{}, error
+	}
+
+	if Category, error := strconv.ParseFloat(parts[5], 64); error == nil {
+		deepSort.Category = Category
+	} else {
+		return DeepSort{}, error
+	}
+
+	if Identity, error := strconv.ParseFloat(parts[9], 64); error == nil {
+		deepSort.Identity = Identity
+	} else {
+		return DeepSort{}, error
+	}
+
+	return deepSort, nil
+}
---
+++ lib_test.go
@@ -0,0 +1,42 @@
+package deepsortParser
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestCreateDeepSort(t *testing.T) {
+	a, error := CreateByLine("368,110.61227191652152,60.857036926171105,141.4988353230947,151.90456305208812,0.0,-2.3347693962743024,0.018604981375242913,-114.70112103062752,331.0")
+	if error != nil {
+		t.Errorf("error must be nil %v", error)
+	} else {
+		fmt.Println(error)
+	}
+	if a.Identity != 331.0 {
+		t.Errorf("ID must be 331.0 but %f", a.Identity)
+	}
+	correct := DeepSort{FrameNo: 368, PosX: 110.61227191652152, PosY: 60.857036926171105,
+		PosW: 141.4988353230947, PosH: 151.90456305208812,
+		Category: 0, Identity: 331}
+
+	if a != correct {
+		t.Errorf("%v but %v", a, correct)
+	}
+
+	b, error := CreateByLine("368.0,110.61227191652152,60.857036926171105,141.4988353230947,151.90456305208812,0.0,-2.3347693962743024,0.018604981375242913,-114.70112103062752,331.0")
+	if error == nil {
+		t.Errorf("error must be not nil %v", error)
+	} else {
+		fmt.Printf("%v\n", error)
+	}
+	fmt.Println(b)
+
+	c, error := CreateByLine("")
+	if error == nil {
+		t.Errorf("error must be not nil %v", error)
+	} else {
+		fmt.Printf("%v\n", error)
+	}
+	fmt.Println(c)
+
+}
